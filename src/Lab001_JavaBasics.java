416a8fc17aba0daa126d049b407c471d792e0b4c
dmothkur <mothukuridivya93@gmail.com>
2025-03-18 19:45:17 +0530

    i am adding java program

--- src/Lab001_JavaBasics.java
+++ src/Lab001_JavaBasics.java
@@ -1,5 +1,10 @@
 public class Lab001_JavaBasics {
     public static void main(String[] args) {
         System.out.println(1+1);
+        System.out.println(1-2);
+        System.out.println(1+3);
+        System.out.println(1+5);
+        System.out.println(1%1);
+        System.out.println(1/1);
     }
 }
